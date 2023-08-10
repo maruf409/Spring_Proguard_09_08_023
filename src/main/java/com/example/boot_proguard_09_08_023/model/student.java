cab8b7ebc6d74cbd2dd95bac1167f4376fd4bfef
MARUF\maruf <maruf.ahmed@nexdecade.com>
2023-08-10 17:56:51 +0600

    added new code

--- src/main/java/com/example/boot_proguard_09_08_023/model/student.java
+++ src/main/java/com/example/boot_proguard_09_08_023/model/student.java
@@ -2,6 +2,38 @@ package com.example.boot_proguard_09_08_023.model;
 
 public class student {
 
+    public Integer getStuId() {
+        return stuId;
+    }
+
+    public void setStuId(Integer stuId) {
+        this.stuId = stuId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
     private Integer stuId;
     private String name;
     private String address;
--- src/main/java/com/example/boot_proguard_09_08_023/model/teacher.java
+++ src/main/java/com/example/boot_proguard_09_08_023/model/teacher.java
@@ -3,6 +3,38 @@ package com.example.boot_proguard_09_08_023.model;
 
 public class teacher {
 
+    public Integer getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(Integer teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public String getTeaName() {
+        return teaName;
+    }
+
+    public void setTeaName(String teaName) {
+        this.teaName = teaName;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
     private Integer teacherId;
     private String teaName;
     private String designation;
